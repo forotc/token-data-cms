3656edf27a87f26e7cadb5a770f9c02d8fbb87cb
Michael <yfg147@163.com>
2018-01-17 19:43:05 +0800

    修复页面样式，图片显示不全问题，添加定时任务重新调用接口保存数据

--- src/main/java/com/ailu/tokenmedia/manage/schedule/PullData.java
+++ src/main/java/com/ailu/tokenmedia/manage/schedule/PullData.java
@@ -1,39 +1,39 @@
 package com.ailu.tokenmedia.manage.schedule;
 
-import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ailu.tokenmedia.manage.account.service.ManageAccountService;
+import com.ailu.tokenmedia.manage.obtain.service.ObtainService;
 
 @Component
 public class PullData {
 	private final static Logger log = LoggerFactory.getLogger(PullData.class);
+	@Autowired
+	ManageAccountService manageAccountService;
+	@Autowired
+	ObtainService obtainService;
 
-	public static void main(String[] args) {
+	public void pull() {
+		List<String> list = manageAccountService.getNull();
+		obtainService.saveWechatInfo(list);
+	}
 
-		String json = "{\"id\":1,\"name\":\"test\"}";
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-			Map<String, Object> map = objectMapper.readValue(json, Map.class);
-			System.out.println("ID:" + map.get("id") + "  " + "name:" + map.get("name"));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	@Scheduled(cron = "0 0 0 * * ? ")
+	private void dayPull() {
+		log.debug(".......pull start");
+		pull();
+		log.debug("pull end.........");
 	}
 
 	// 每周一 0点0分0秒 执行
-	@Scheduled(cron = "0 0 0 ? * MON ")
-	private void overdue() {
-		log.debug(".......卡过期check start");
-		// TODO
-		log.debug("卡过期check end.........");
-	}
+	/*
+	 * @Scheduled(cron = "0 0 0 ? * MON ") private void overdue() {
+	 * log.debug(".......start"); // TODO log.debug(" end........."); }
+	 */
 }
